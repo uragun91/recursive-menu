713dd1bbefa1a12f15f07a18679e0ecfd4824e1d
Ivan Frolov <i.frolov@draewil.com>
2020-06-15 23:24:55 +0300

    Implemented reactive form

--- src/app/menu/components/section/section.component.ts
+++ src/app/menu/components/section/section.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { MenuService } from '../../services/menu.service';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-section',
@@ -10,11 +11,18 @@ import { MenuService } from '../../services/menu.service';
 export class SectionComponent implements OnInit {
 
   public allSections: string[] = []
+  public sectionForm: FormGroup
 
   constructor(
     private menuSerive: MenuService,
-    private cd: ChangeDetectorRef
-  ) { }
+    private cd: ChangeDetectorRef,
+    private fb: FormBuilder
+  ) {
+    this.sectionForm = this.fb.group({
+      'name': ['', [Validators.required]],
+      'section': ['', [Validators.required]]
+    })
+  }
 
   ngOnInit() {
     this.menuSerive.getSections()
